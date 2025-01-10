fc277ba019472083113913c15ebdccda298857fc
FraProse23 <fra.prose23@gmail.com>
2025-01-10 10:19:57 +0000

    mm

---
+++ vector.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+    int m[2][2];
+    int m1[2][2];
+    int max=0;
+    cout<<"inserisci una matrice 2x2 "<<endl;
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            cout<<"inserisci l'elemento della prima matrice "<<i<<" "<<j<<endl;
+            cin>>m[i][j];
+            
+              
+            
+        }}
+        for(int i=0;i<2;i++)
+     {
+        for(int j=0;j<2;j++)
+        {
+            if(m[i][j]==3){
+                cout<<" Il numero 3 è presente  " <<endl;
+
+            } else {
+              cout<<" Non è presente il numero 3  " <<endl;
+            }
+
+        
+        
+        }}
