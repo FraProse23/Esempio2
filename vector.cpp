4f133ba95f94f3002fb2a5516c3b85ef0b18df46
FraProse23 <fra.prose23@gmail.com>
2025-01-10 11:35:22 +0100

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,17 +1,20 @@
-#include <iostream>
+#include <iostream> #include <random> 
+
 using namespace std;
 int main()
 { 
     int m[2][2];
-    int m1[2][2];
+    
     int max=0;
     cout<<"inserisci una matrice 2x2 "<<endl;
     for(int i=0;i<2;i++)
     {
         for(int j=0;j<2;j++)
         {
-            cout<<"inserisci l'elemento della prima matrice "<<i<<" "<<j<<endl;
-            cin>>m[i][j];
+                int >m[i][j]; = std::rand();
+
+          
+           
             
               
             
